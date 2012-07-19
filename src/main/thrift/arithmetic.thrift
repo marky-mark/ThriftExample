1357f6107ce20ac5d4f9242ea3344a6770509e61
mkelly <mkelly28@tcd.ie>
2012-07-19 16:14:43 +0100

    first commit

---
+++ src/main/thrift/arithmetic.thrift
@@ -0,0 +1,11 @@
+namespace java tutorial.arithmetic.gen  // define namespace for java code
+
+//This is basically just C
+
+typedef i64 long
+typedef i32 int
+
+service ArithmeticService {  // defines simple arithmetic service
+    long add(1:int num1, 2:int num2),
+    long multiply(1:int num1, 2:int num2),
+}
